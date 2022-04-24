dfc824e0d8842dfc7e75bddc677781da013c53a6
Hans Daduya <hansdaduya@yahoo.com>
2022-04-24 21:06:44 +0800

    :sparkles: Set up prisma nexus for graphql queries
    Set up prisma nexus for graphql queries

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -4,49 +4,9 @@ const prisma = new PrismaClient();
 
 const userData: Prisma.UserCreateInput[] = [
 	{
-		name: "Alice",
-		email: "alice@prisma.io",
-		posts: {
-			create: [
-				{
-					title: "Join the Prisma Slack",
-					content: "https://slack.prisma.io",
-					published: true,
-				},
-			],
-		},
-	},
-	{
-		name: "Nilu",
-		email: "nilu@prisma.io",
-		posts: {
-			create: [
-				{
-					title: "Follow Prisma on Twitter",
-					content: "https://www.twitter.com/prisma",
-					published: true,
-					viewCount: 42,
-				},
-			],
-		},
-	},
-	{
-		name: "Mahmoud",
-		email: "mahmoud@prisma.io",
-		posts: {
-			create: [
-				{
-					title: "Ask a question about Prisma on GitHub",
-					content: "https://www.github.com/prisma/prisma/discussions",
-					published: true,
-					viewCount: 128,
-				},
-				{
-					title: "Prisma on YouTube",
-					content: "https://pris.ly/youtube",
-				},
-			],
-		},
+		userType: "ADMIN",
+		username: "eulermacaroni",
+		password: "123",
 	},
 ];
 
