64773a5e191f7fce3d6d4c79539e09a67b111c26
Hans Daduya <hansdaduya@yahoo.com>
2022-04-27 02:04:58 +0800

    :construction: Create patient list table
    Create patient list table

--- frontend/src/components/PatientList/PatientList.tsx
+++ frontend/src/components/PatientList/PatientList.tsx
@@ -1,7 +1,110 @@
-import React from 'react'
+import * as React from 'react'
+import Link from '@mui/material/Link'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
 
-function PatientList() {
-  return <div>PatientList</div>
+// Generate Order Data
+function createData(
+  lastVisit: string,
+  name: string,
+  gender: string,
+  contactNum: string,
+  visitType: string,
+  doctor: string,
+) {
+  return {
+    lastVisit,
+    name,
+    gender,
+    contactNum,
+    visitType,
+    doctor,
+  }
 }
 
-export default PatientList
+const rows = [
+  // createData example
+  createData(
+    '01/04/22',
+    'Ronald Richards',
+    'Male',
+    '0123456789',
+    'Doctor Visit',
+    'Dr. Theresa Web',
+  ),
+  createData(
+    '01/04/22',
+    'Ronald Richards',
+    'Male',
+    '0123456789',
+    'Doctor Visit',
+    'Dr. Theresa Web',
+  ),
+  createData(
+    '01/04/22',
+    'Ronald Richards',
+    'Male',
+    '0123456789',
+    'Doctor Visit',
+    'Dr. Theresa Web',
+  ),
+  createData(
+    '01/04/22',
+    'Ronald Richards',
+    'Male',
+    '0123456789',
+    'Doctor Visit',
+    'Dr. Theresa Web',
+  ),
+  createData(
+    '01/04/22',
+    'Ronald Richards',
+    'Male',
+    '0123456789',
+    'Doctor Visit',
+    'Dr. Theresa Web',
+  ),
+]
+
+function preventDefault(event: React.MouseEvent) {
+  event.preventDefault()
+}
+
+export default function PatientsList() {
+  return (
+    <>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Last Visited</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Gender</TableCell>
+            <TableCell>Contact Number</TableCell>
+            <TableCell>Visit Type</TableCell>
+            <TableCell>Doctor</TableCell>
+            <TableCell align="right" />
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell>{row.lastVisit}</TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.gender}</TableCell>
+              <TableCell>{row.contactNum}</TableCell>
+              <TableCell>{row.visitType}</TableCell>
+              <TableCell>{row.doctor}</TableCell>
+              <TableCell align="right">Details</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
+        See more orders
+      </Link>
+    </>
+  )
+}
--- frontend/src/pages/PatientPage/PatientPage.tsx
+++ frontend/src/pages/PatientPage/PatientPage.tsx
@@ -16,6 +16,7 @@ import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
 
 import Sidebar from '../../components/Sidebar'
 import Header from '../../components/Header'
+import PatientsList from '../../components/PatientList'
 
 function Copyright(props: any) {
   return (
@@ -102,36 +103,10 @@ function DashboardContent() {
           <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Header />
             <Grid container spacing={3}>
-              {/* Chart */}
-              <Grid item xs={12} md={8} lg={9}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}
-                >
-                  Chart
-                </Paper>
-              </Grid>
-              {/* Recent Deposits */}
-              <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}
-                >
-                  Deposits
-                </Paper>
-              </Grid>
               {/* Recent Orders */}
               <Grid item xs={12}>
                 <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                  Orders
+                  <PatientsList />
                 </Paper>
               </Grid>
             </Grid>
