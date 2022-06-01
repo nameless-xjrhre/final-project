b3b3fbe26ac120d3d3c6da79c65f7fa5a3416898
EdmelKun <elinaugo137@gmail.com>
2022-06-02 05:09:25 +0800

    refactor:edit bill test

--- frontend/cypress/integration/editBill.spec.ts
+++ frontend/cypress/integration/editBill.spec.ts
@@ -1,17 +1,28 @@
-describe('Bills Page - Edit Bills Test',()=>{
-    it('should change amount',()=>{
+describe('Bills Page - Edit Bills Test', () => {
+    before(() => {
         cy.visit('http://localhost:3000/bills')
-        .get('[id=basic-button]').eq(6).click()
-        .get('[role="menuitem"]').contains('Edit').click({force:true})
-        .get('[name=amount]').last().click().type('6969')
     })
 
-    it('should select payment term',()=>{
+    it('should change amount', () => {
+        cy.get('[id=basic-button]').eq(7).click()
+            .get('[role="menuitem"]').contains('Edit').click({ force: true })
+            .get('[name=amount]').last().click().type('42069')
+    })
+
+    it('should select payment term', () => {
         cy.get('[id=mui-component-select-paymentTerm]').last().click()
-        .get('[data-value="60 days"]').click()
+            .get('[data-value="15 days"]').click()
     })
 
-    it('should save changes',()=>{
-        cy.get('[type=button]').last().contains('Save Changes').click()
+    it('should save changes', () => {
+        cy.get('[type=button]').last().contains('Save Changes').click().wait(5000)
     })
+
+    it('should display confirmation message', () => {
+        cy.get('[class="swal-title"]')
+          .should('contain', 'Success')
+          .get('[class="swal-button swal-button--confirm"]')
+          .contains('OK')
+          .click()
+      })
 })
