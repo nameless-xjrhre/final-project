a384626cacf9b713171cf42f1067a0258a32d4e9
EdmelKun <elinaugo137@gmail.com>
2022-06-02 05:06:59 +0800

    refactor:add doctor test

--- frontend/cypress/integration/addDoctor.spec.ts
+++ frontend/cypress/integration/addDoctor.spec.ts
@@ -1,21 +1,32 @@
-describe('Doctor Page - Add Doctor Test', ()=>{
-    it('should input name of doctor',()=>{
+describe('Doctor Page - Add Doctor Test', () => {
+    before(() => {
         cy.visit('http://localhost:3000/doctors')
-        .get('[aria-label="SpeedDial basic example"]').trigger('mouseover')
-        .get('[aria-label="Add Doctor"]').click()
-        .get('[name=firstName]').type('Stuart')
-        .get('[name=lastName]').type('Dolittle')
     })
 
-    it('should input contact information',()=>{
+    it('should input name of doctor', () => {
+        cy.get('[aria-label="SpeedDial basic example"]').trigger('mouseover')
+            .get('[aria-label="Add Doctor"]').click()
+            .get('[name=firstName]').type('Shinobu')
+            .get('[name=lastName]').type('Kuki')
+    })
+
+    it('should input contact information', () => {
         cy.get('[name=contactNum]').type('09273877110')
     })
 
-    it('should input address',()=>{
+    it('should input address', () => {
         cy.get('[name=address]').type('Naga, Camarines Sur')
     })
 
     it('should click add doctor button', () => {
-        cy.get('button').contains('Add Doctor').click()
+        cy.get('button').contains('Add Doctor').click().wait(5000)
+    })
+
+    it('should display confirmation message', ()=>{
+        cy.get('[class="swal-title"]')
+        .should('contain', 'Success')
+        .get('[class="swal-button swal-button--confirm"]')
+        .contains('OK')
+        .click()
     })
 })
