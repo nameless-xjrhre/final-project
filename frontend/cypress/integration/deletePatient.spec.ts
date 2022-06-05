6cf6bb5c429068d7e3df884f707163dddef4a1c9
EdmelKun <elinaugo137@gmail.com>
2022-06-05 19:09:26 +0800

    refactor:delete patient test

--- frontend/cypress/integration/deletePatient.spec.ts
+++ frontend/cypress/integration/deletePatient.spec.ts
@@ -5,7 +5,7 @@ describe('Patient Page - Delete Patient Test', () => {
 
     it('should delete patient', () => {
         cy.get('[id="basic-button"]').eq(6).click()
-            .get('ul').get('li').last().contains('Delete').click()
+            .get('[role="menuitem"]').last().contains('Delete').click()
             .get('[type=button]').last().contains('Yes').click()
             .wait(5000)
     })
