fe9039b616b0fda96551115d5a20d980abe8e62f
EdmelKun <elinaugo137@gmail.com>
2022-06-06 10:45:04 +0800

    fix:added fake data

--- frontend/cypress/integration/viewNote.spec.ts
+++ frontend/cypress/integration/viewNote.spec.ts
@@ -1,10 +1,12 @@
+import {patientRandomizer} from "../fixtures/randomizer"
+
 describe('Appointment Page - View Note Test', ()=>{
     before(() => {
         cy.visit('http://localhost:3000/appointments')
     })
 
     it('should hover view note button', () => {
-        cy.get('[id="basic-button"]').eq(7)
+        cy.get('[id="basic-button"]').eq(patientRandomizer())
             .click()
             .get('[role="menuitem"]')
             .contains('View Note')
