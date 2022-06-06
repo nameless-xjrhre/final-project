e67f4f6fd455ee0cb880e0d30a8da7b29742163a
EdmelKun <elinaugo137@gmail.com>
2022-06-06 10:43:15 +0800

    fix:added fake data

--- frontend/cypress/integration/editAppointment.spec.ts
+++ frontend/cypress/integration/editAppointment.spec.ts
@@ -1,44 +1,55 @@
+import { fakeDataRandomizer, patientRandomizer, statusRandomizer, medStaffRandomizer, visitTypeRandomizer} from "../fixtures/randomizer"
+const fakeData = require('../fixtures/fakeData.json')
+const fakeDataProps = fakeData.listOfObjects
+
 describe('Appointment Page - Edit Appointment Test', () => {
     before(() => {
         cy.visit('http://localhost:3000/appointments')
     })
 
     it('should click edit button', () => {
-        cy.get('[id="basic-button"]').eq(5)
+        cy.get('[id="basic-button"]').eq(patientRandomizer())
             .click()
+            .wait(3000)
             .get('[role="menuitem"]').contains('Edit').click({ force: true })
     })
 
     it('should change status', () => {
         cy.get('[id=mui-component-select-status]').last().click()
-            .get('[data-value="EXPIRED"]').click()
+            .get('[data-value='+ statusRandomizer() + ']')
+            .click()
+            .wait(3000)
     })
 
     it('should select visit type', () => {
         cy.get('[id=mui-component-select-visitType]').last()
             .click()
-            .get('[data-value=ROUTINE]')
+            .wait(3000)
+            .get('[name=visitType]')
+            .contains(visitTypeRandomizer())
             .click()
     })
 
     it('should select medical staff', () => {
         cy.get('[id=mui-component-select-medicalStaff]').last()
             .click()
-            .get('[name="medicalStaff"]')
-            .last()
+            .wait(3000)
+            .get('[role="option"]')
+            .eq(medStaffRandomizer())
             .click()
     })
 
     it('should input date', () => {
         cy.get('[name=appointmentDate]').last()
             .click()
-            .type('10/06/2022')
+            .type(fakeDataProps[fakeDataRandomizer()].appointmentDate)
             .clear()
-            .type('10/31/2022')
+            .type(fakeDataProps[fakeDataRandomizer()].appointmentDate)
     })
 
     it('should input time', () => {
-        cy.get('[name=appointmentTime]').last().click().clear().type('16:30')
+        cy.get('[name=appointmentTime]').last().click().clear()
+        .type(fakeDataProps[fakeDataRandomizer()].startTime)
     })
 
     it('should input note', () => {
@@ -51,6 +62,7 @@ describe('Appointment Page - Edit Appointment Test', () => {
 
     it('should display confirmation message', () => {
         cy.get('[class="swal-title"]')
+            .wait(2000)
             .should('contain', 'Success')
             .get('[class="swal-button swal-button--confirm"]')
             .contains('OK')
