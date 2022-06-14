f4c8cdef6bc0fa471e01b053b33bdccb8d88427b
unknown <dierongamer04@gmail.com>
2022-06-14 15:44:07 +0800

    test: added more tests in TabPanel

--- frontend/src/components/TabPanel/TabPanel.test.tsx
+++ frontend/src/components/TabPanel/TabPanel.test.tsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom'
 import { screen } from '@testing-library/react'
 import { testRenderer } from '../../utils/test-util'
+import user from '@testing-library/user-event'
 import BasicTabs from './TabPanel'
 import { graphql } from 'msw'
 import {
@@ -47,6 +48,13 @@ describe('TabSkeletPanel', () => {
       ),
     )
 
+    const medRecordLabel = screen.getByRole('tab', {
+      name: /medical records/i,
+    })
+    expect(medRecordLabel.innerHTML).toContain('Medical Records')
+
+    user.click(medRecordLabel)
+
     const date = screen.getByText(/date/i)
     expect(date.innerHTML).toBe('Date')
 
@@ -58,5 +66,30 @@ describe('TabSkeletPanel', () => {
 
     const prescription = screen.getByText(/prescription/i)
     expect(prescription.innerHTML).toBe('Prescription')
+
+    const financialRecordLabel = screen.getByRole('tab', {
+      name: /financial records/i,
+    })
+    expect(financialRecordLabel.innerHTML).toContain('Financial Records')
+
+    user.click(financialRecordLabel)
+
+    const paymentDate = screen.getByText(/payment date/i)
+    expect(paymentDate.innerHTML).toBe('Payment Date')
+
+    const staff = screen.getByText(/staff/i)
+    expect(staff.innerHTML).toBe('Staff')
+
+    const dueDate = screen.getByText(/due date/i)
+    expect(dueDate.innerHTML).toBe('Due Date')
+
+    const amount = screen.getByText(/amount/i)
+    expect(amount.innerHTML).toBe('Amount ')
+
+    const status = screen.getByText(/status/i)
+    expect(status.innerHTML).toBe('Status')
+
+    const visitType = screen.getByText(/visit type/i)
+    expect(visitType.innerHTML).toBe('Visit Type ')
   })
 })
